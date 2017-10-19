39a4d7b09b0a86f2b114b9a821eb21da6daf3bc5
Mustafa Serkan Dokuzcan <MustafaSerkan.Dokuzcan@cgm.com>
2017-10-19 21:49:30 +0200

    test fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,16 @@
+import { HttpModule } from '@angular/http';
+import { GithubSearchService } from './search/github.service';
+import { SearchComponent } from './search/search.component';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [ AppComponent, SearchComponent],
+      imports: [ReactiveFormsModule, HttpModule],
+      providers: [GithubSearchService]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -17,11 +22,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,3 +1,6 @@
+import { HttpModule } from '@angular/http';
+import { GithubSearchService } from './github.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchComponent } from './search.component';
@@ -8,7 +11,9 @@ describe('SearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+      declarations: [ SearchComponent ],
+      imports: [ReactiveFormsModule, HttpModule],
+      providers: [GithubSearchService]
     })
     .compileComponents();
   }));
