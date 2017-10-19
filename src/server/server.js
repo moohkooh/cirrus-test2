56133f4a3e99708e8a4346c5af9cd53f1b213563
Mustafa Serkan Dokuzcan <MustafaSerkan.Dokuzcan@cgm.com>
2017-10-19 23:45:23 +0200

    dist path

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/dist'));
+console.log(__dirname + '../../dist');
+
+app.use(express.static(__dirname + '/../../dist'));
 
 app.listen(process.env.PORT || 8080, function () {
     console.log('Example app listening on port 8080!');
