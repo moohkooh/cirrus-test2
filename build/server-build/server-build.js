04efdb44c2c40a18650fc2625576fe2c1808b7ac
Mustafa Serkan Dokuzcan <MustafaSerkan.Dokuzcan@cgm.com>
2017-10-24 01:18:46 +0200

    compress

--- build/server-build/server-build.js
+++ build/server-build/server-build.js
@@ -4,8 +4,13 @@ compressor.minify({
     compressor: 'uglifyjs',
     input: '../../src/server/server.js',
     output: '../../dist/server.js',
-    callback: function (err, min) { 
-        if(err){
+    options: {
+        warnings: true, // pass true to display compressor warnings.
+        mangle: false, // pass false to skip mangling names.
+        compress: true // pass false to skip compressing entirely. Pass an object to specify custom compressor options.
+    },
+    callback: function (err, min) {
+        if (err) {
             console.error(err);
         }
         console.info("Build success!");
