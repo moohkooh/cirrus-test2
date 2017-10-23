adc8f7597f00c449661d31651d779bc1153ff81a
Mustafa Serkan Dokuzcan <MustafaSerkan.Dokuzcan@cgm.com>
2017-10-23 15:08:20 +0200

    build on server

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-console.log(__dirname + '/../../dist');
-
-app.use(express.static(__dirname + '/../../dist'));
+app.use(express.static(__dirname + '../dist'));
 
 app.listen(process.env.PORT || 8080, function () {
     console.log('Example app listening on port 8080!');
